7fbd115115f65ce72e7a0184ff32a40e45e43996
Andrey Semakin <and-semakin@ya.ru>
2018-04-17 16:59:43 +0500

    Add comments from users, not from 'gitlab' account. Add GitLab project name as prefix to issues.

--- get_issues.py
+++ get_issues.py
@@ -59,6 +59,7 @@ def get_user_by_login(login):
 
 # youtrack project
 yt_project_id = 'ezmp'
+# yt_project_id = 'youtrack_test'
 print(yt_project_id)
 # create youtrack project if not exists
 try:
@@ -75,7 +76,8 @@ projects = requests.get(f'{gitlab_url}projects?per_page={gitlab_per_page}&privat
 for p in projects:
     project_id = p['id']
     project_path = p['path']
-    issues = requests.get(f'{gitlab_url}projects/{project_id}/issues?scope=all&per_page={gitlab_per_page}&private_token={gitlab_api_token}').json()
+    issues = requests.get(f'{gitlab_url}projects/{project_id}/issues?scope=all'
+                          f'&per_page={gitlab_per_page}&private_token={gitlab_api_token}').json()
 
     # print project name
     print()
@@ -87,15 +89,17 @@ for p in projects:
     for issue in issues:
         issue_iid = issue['iid']
         assignee = get_user_by_id(issue['assignee']['id']) if issue['assignee'] is not None else None
-        summary = issue['title']
+        summary = "[" + project_path + "] " + issue['title']
         description = issue['description']
-        response = youtrack.create_issue(yt_project_id, assignee, summary, description)
+        state = 'fixed' if issue['state'] == 'closed' else None
+        response = youtrack.create_issue(yt_project_id, assignee, summary, description, state=state)
         yt_issue_id = response[0]['location'].split('/')[-1]
         # pprint(issue)
         print(" *", summary)
 
         comments = requests.get(
-            f'{gitlab_url}projects/{project_id}/issues/{issue_iid}/notes?per_page={gitlab_per_page}&private_token={gitlab_api_token}').json()
+            f'{gitlab_url}projects/{project_id}/issues/{issue_iid}/'
+            f'notes?per_page={gitlab_per_page}&private_token={gitlab_api_token}').json()
         for comment in comments:
             username = comment['author']['name']
             text = comment['body']
@@ -103,6 +107,6 @@ for p in projects:
 
             full_text = f"{username} ({created_at}):\n\n{text}"
 
-            youtrack.execute_command(yt_issue_id, 'comment', full_text)
+            youtrack.execute_command(yt_issue_id, 'comment', full_text, run_as=get_user_by_id(comment['author']['id']))
             # pprint(comment)
             print('   *', username, created_at)
