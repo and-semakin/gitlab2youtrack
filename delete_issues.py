44538ebb62354db492cc3306021b5f094eed95d2
Andrey Semakin <and-semakin@ya.ru>
2018-07-11 20:17:41 +0500

    Обновил список проектов для очистки

--- delete_issues.py
+++ delete_issues.py
@@ -5,7 +5,19 @@ youtrack_login = 'GitLab'
 youtrack_password = 'gitlab'
 youtrack = Connection(youtrack_url, youtrack_login, youtrack_password)
 
-project_list = ['ezmp', 'youtrack_test']
+project_list = [
+    'ezmp',
+    'chel_taxi',
+    'chel_medicine',
+    'ekb_taxi',
+    'ekb_medicine',
+    'ekb_alcohol',
+    'ekb_state_property',
+    'ekb_building',
+    'ekb_home_book',
+    'ekb_investments',
+    'ekb_advertising'
+]
 
 for _ in range(30):
     for p in project_list:
